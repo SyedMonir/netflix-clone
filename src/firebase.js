7f5eaf85423b5dfef8e7b2c81c8c7f46a5a728ad
Syed Monirul Alam <Rambling.ctg@gmail.com>
2021-12-29 23:12:46 +0600

    version-1

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjAdZZgB_DchMA-6cYVeM0Yu3w6PPhj1c",
+    authDomain: "netflix-clone-85e89.firebaseapp.com",
+    projectId: "netflix-clone-85e89",
+    storageBucket: "netflix-clone-85e89.appspot.com",
+    messagingSenderId: "905361297259",
+    appId: "1:905361297259:web:3b9d078152c25b1996c62e"
+};
